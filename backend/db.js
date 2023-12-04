dc610f5fbdf9ef3f7cd7463d08715bb676fcb1dc
Xrustofor <xrustofor84@gmail.com>
2023-12-04 21:05:30 +0200

    finish version 1.5

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const password = 'gn1FFVI8Lr9gXritowttekXt8dUFHibU'; //'ouzD6984';
 
 
 const sequelize = new Sequelize(database, username, password, {
-    host: 'localhost',
+    host: 'dpg-cln1jm4jtl8s73abhg6g-a',
     dialect: 'postgres',
     port: '5432'
 })
