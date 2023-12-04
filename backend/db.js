ea9e3cc2c6384fbe5e07bc42ad879e53288c7007
Xrustofor <xrustofor84@gmail.com>
2023-12-04 20:57:55 +0200

    finish version 1.2

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const password = 'gn1FFVI8Lr9gXritowttekXt8dUFHibU'; //'ouzD6984';
 
 
 const sequelize = new Sequelize(database, username, password, {
-    host: 'https://promo-qag1.onrender.com',
+    host: 'localhost',
     dialect: 'postgres',
     port: '5432'
 })
